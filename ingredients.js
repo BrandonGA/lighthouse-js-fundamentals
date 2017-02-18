7f46739f93a645f84d048251e913826b770555bb
Brandon Galea-Andrews <brandon.galeaandrews@gmail.com>
2017-02-18 16:17:53 +0000

    Adds looping exercises

---
+++ ingredients.js
@@ -0,0 +1,18 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var repeat = 0
+while (repeat = 0); {
+console.log(ingredients);
+}
+
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+console.log("Banana Bread Recipe");
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+for (var i = ingredients.lenght-1; i>=0; i--){
+  console.log(ingredient[i]);
+}
